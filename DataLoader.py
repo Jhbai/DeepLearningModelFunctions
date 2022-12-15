81255cbb26645c25d9f48277bbdd603dbbfcd15f
105071079 <37044030+Jhbai@users.noreply.github.com>
2022-12-15 20:08:52 +0800

    Create DataLoader.py

---
+++ DataLoader.py
@@ -0,0 +1,11 @@
+def dataloader(batch_size:int, original_data:torch.tensor) -> list:
+    N = original_data.shape[0]
+    alist = [i for i in range(N)]
+    result = list()
+    np.random.shuffle(alist)
+    for i in range(N//batch_size):
+        if (i+1)*batch_size >= N:
+            result.append(original_data[i*batch_size:])
+        else:
+            result.append(original_data[i*batch_size:(i+1)*batch_size])
+    return result
