9d14333d799b9163b273270bbbd6c7d1923ad43e
105071079 <37044030+Jhbai@users.noreply.github.com>
2022-12-15 18:40:55 +0800

    Update DCGAN模型.py

--- DCGAN模型.py
+++ DCGAN模型.py
@@ -27,7 +27,7 @@ def Draw(array):
     plt.imshow((array*0.5 + 0.5).permute(1,2,0).detach().to(device = 'cpu'))
     plt.axis('off')
 def multi_Draw(Input):
-    flat = 1
+    flag = 1
     for obj in Input:
         plt.subplot(1, 5, flag)
         Draw(obj.to(device = 'cpu'))
